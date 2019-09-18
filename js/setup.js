9326163ac07e1f11ec019704e0372fd1d7628efc
galla-okto <galla.okto1@gmail.com>
2019-09-18 23:30:45 +0300

    Добавляет выбор магов

---
+++ js/setup.js
@@ -0,0 +1,55 @@
+'use strict';
+
+var WIZARD_NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
+var WIZARD_SURNAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
+var WIZARD_COAT_COLOR = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
+var WIZARD_EYES_COLOR = ["black", "red", "blue", "yellow", "green"];
+
+var userDialog = document.querySelector('.setup');
+userDialog.classList.remove('hidden');
+
+var similarListElement = userDialog.querySelector('.setup-similar-list');
+
+var similarWizardTemplate = document.querySelector('#similar-wizard-template')
+    .content
+    .querySelector('.setup-similar-item');
+
+var wizards = [];
+
+var getRandom = function (max) {
+  return Math.round(Math.random() * max);
+};
+
+var getWizards = function () {
+  for (var i = 0; i < 4; i++) {
+
+    wizards.push({name: WIZARD_NAMES[getRandom(WIZARD_NAMES.length - 1)] + ' ' + WIZARD_SURNAMES[getRandom(WIZARD_SURNAMES.length - 1)],
+                  coatColor: WIZARD_COAT_COLOR[getRandom(WIZARD_COAT_COLOR.length - 1)],
+                  eyesColor: WIZARD_EYES_COLOR[getRandom(WIZARD_EYES_COLOR.length - 1)]});
+  };
+};
+
+getWizards();
+
+var renderWizard = function (wizard) {
+  var wizardElement = similarWizardTemplate.cloneNode(true);
+
+  wizardElement.querySelector('.setup-similar-label').textContent = wizard.name;
+  wizardElement.querySelector('.wizard-coat').style.fill = wizard.coatColor;
+  wizardElement.querySelector('.wizard-eyes').style.fill = wizard.eyes;
+
+  return wizardElement;
+};
+
+var showWizard = function () {
+  var fragment = document.createDocumentFragment();
+
+  for (var i = 0; i < wizards.length; i++) {
+    fragment.appendChild(renderWizard(wizards[i]));
+  }
+  similarListElement.appendChild(fragment);
+};
+
+showWizard();
+
+userDialog.querySelector('.setup-similar').classList.remove('hidden');
