b0baf71abdf4586e2877006c1398b4fc7c34067f
galla-okto <galla.okto1@gmail.com>
2019-10-16 20:30:58 +0300

    Добавляет переменным глобальную область видимости

--- js/dialog.js
+++ js/dialog.js
@@ -4,8 +4,6 @@
 
   var MIN_LENGTH_NAME = 2;
 
-  var WIZARD_COAT_COLOR = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
-  var WIZARD_EYES_COLOR = ['black', 'red', 'blue', 'yellow', 'green'];
   var WIZARD_FIREBALL_COLOR = ['#ee4830', '#30a8ee', '#5ce6c0', '#e848d5', '#e6e848'];
 
   var setup = document.querySelector('.setup');
